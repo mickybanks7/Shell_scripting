798bd93887aff43b9aa282a44cab1c29c3df90e6
mickybanks7 <mjbanks4real@gmail.com>
2023-04-04 14:09:27 +0100

    Additional codes

--- README.md
+++ README.md
@@ -6,6 +6,18 @@ I introduced different concepts and will constantly update this repo.
 
 You can fork, pull or clone the repo if you want a resources that is easy to understand.
 
+**Please Note the following must be done**
+
+1.  #!/bin/bash  (As seen in the shell script file, is what tells your system the interpreter to use).
+2.  You must use this code on your terminal before your script works.
+    
+    which bash (enables you see the location of your interpreter - /bin/bash)
+    
+    chmod +x *file name.sh* ( allows executable permission)
+    
+    ./filename.sh
+    
+
 
 
 Happy Learning..........
--- tutorials.sh
+++ tutorials.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/bash                     
 #echo hello world!
 
 
 #ECHO COMMAND
 #echo hello word
 
+#VARIABLE NAME
 #Name="Emeka"
 #echo "My name is $Name"
 
@@ -35,7 +36,7 @@
 
 
 #ELSE-IF (ELIF)
-Name="Michael"
+#Name="Michael"
 #if [ "$Name" == "Emeka" ]
 #then
  #   echo "Your name is $Name"
@@ -50,15 +51,15 @@ Name="Michael"
 
 
 #COMPARISONS
-num1=5
-num2=10
+#num1=5
+#num2=10
 
-if [ "$num1" -gt "$num2" ]
-then
-    echo "$num1 is greater than $num2"
-else
-    echo "$num1 is less than $num2"
-fi
+#if [ "$num1" -gt "$num2" ]
+#then
+ #   echo "$num1 is greater than $num2"
+#else
+ #   echo "$num1 is less than $num2"
+#fi
 #SYNTAX AND EXPLANATION
 # num1 -eq num2 (Returns true if number 1 is equal to number 2)
 # num1 -nq num2 (Returns true if number 1 is not equal to number 2)
@@ -70,6 +71,24 @@ fi
 
 
 #FILE CONDITIONS
+#File="check.txt"
+#if [ -f "$File" ]
+#then
+ #   echo "$File exists in our folder"
+#else
+ #   echo "$File does not exist in our folder"
+#fi
+
+#on your terminal enter the below syntax:
+    #touch check.txt
+
+#File="check.txt"
+#if [ -f "$File" ]
+#then
+ #   echo "$File exists in our folder"
+#else
+ #   echo "$File does not exist in our folder"
+#fi
 
 # -d file   True if the file is a directory
 # -e file   True if the file exists (note that this is not particularly portable, thus -f is generally used)
@@ -80,3 +99,65 @@ fi
 # -u file   True if the user id is set on a file
 # -w file   True if the file is writable
 # -x file   True if the file is an executable file
+
+
+#CASE STATEMENT
+#read -p "Are you a male or a female?: (M/F)" Answer
+#case "$Answer" in
+ #   [fF] | [eE][mM][aA][lL][eE])
+  #      echo "You can have the wig ):"
+   #     ;;
+    #[mM] | [aA][lL][eE])
+     #   echo "Sorry no, you are a male kindly go have a shave ):"
+      #  ;;
+   # *)
+    #    echo "Please enter as a male or a female"
+     #   ;;
+#esac
+
+
+
+#FOR LOOP
+#Name="Adura Okon John Festus"
+#for Name in $Name
+ #   do
+  #      echo "Hello $Name"
+#done
+
+#Example Two - RENAMING OF FILE NAME
+
+#Files=$(ls *.txt)
+#New="File"
+#for File in $Files
+ #   do
+  #      echo "Renaming $File as new-$File"
+   #     mv $File $New-$File
+#done
+
+
+
+#WHILE LOOP - READ THROUGH A FILE LINE BY LINE
+#Line=1
+#w
+#while read -r Current_line
+ #   do
+ #       echo "$Line: $Current_line"
+  #      ((Line++))
+#done < "./File-fil1.txt"
+
+
+
+#FUNCTIONS
+#read -p "Enter your name: " Name
+#echo "my name is $Name"
+#read -p "Please enter your Name: " Name
+#function sayhello() {
+ #   echo "Hello $Name"
+#}
+#sayhello
+
+#FUNCTIONS WITH PARAMS
+function bio() {
+    echo "My name is $1 and I am a $2"
+}
+bio "Banks" "12"
